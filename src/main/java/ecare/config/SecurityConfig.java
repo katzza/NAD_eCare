57eb8affa98656e959c8d08455ecaa799b194578
emokeeva <ekaterina.mokeeva@t-systems.com>
2023-01-03 23:22:37 +0100

    data from cource

--- src/main/java/ecare/config/SecurityConfig.java
+++ src/main/java/ecare/config/SecurityConfig.java
@@ -47,7 +47,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
