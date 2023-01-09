f582becf176480ceddbd28e406762784c38fffc7
emokeeva <ekaterina.mokeeva@t-systems.com>
2023-01-09 20:27:02 +0100

    start flyway

--- src/main/java/ecare/dto/UserDto.java
+++ src/main/java/ecare/dto/UserDto.java
@@ -2,7 +2,6 @@ package ecare.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
